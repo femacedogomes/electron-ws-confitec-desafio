49c8098fbd70d27635a622dc3c39e0c13f9600ba
Felipe Macedo Gomes <femacedogomes@gmail.com>
2025-01-20 10:49:11 -0300

    feat: install and first configuration ws

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const WebSocket = require('ws');
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -9,15 +10,42 @@ const createWindow = () => {
     win.loadFile('index.html')
   }
 
+const startWebSocketServer = () => {
+  const wss = new WebSocket.Server({ port: 8080 });
+
+  console.log('Servidor Websocket iniciado na porta ws://localhost:8080');
+
+  wss.on('connection', (socket) => {
+    console.log('Cliente conectado');
+
+    socket.on('message', (message) => {
+      console.log(`Recebido: ${message}`);
+
+      // Broadcast para todos os clientes conectados
+      wss.clients.forEach((client) => {
+        if (client !== socket && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    });
+
+    socket.on('close', () => {
+      console.log('Client disconnected');
+    });
+  });
+}
+
   app.whenReady().then(() => {
     createWindow()
-
-    app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') app.quit()
-    })
+    startWebSocketServer()
+    
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
       })
+    
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') app.quit()
+    })
 
   })
 
